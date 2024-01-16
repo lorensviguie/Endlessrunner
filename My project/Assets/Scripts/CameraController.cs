098f5aac97ee2c14c7dd0801f0ceabfd5cb777e4
lorensviguie <lorens.viguie@ynov.com>
2024-01-16 09:16:41 +0100

    coin

--- My project/Assets/Scripts/CameraController.cs
+++ My project/Assets/Scripts/CameraController.cs
@@ -1,25 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    private UnityEngine.Vector3 offset;
+    private Vector3 offset;
+
     void Start()
     {
-      
         offset = transform.position - target.position;
-       
-
     }
 
-   // Update is called once per frame
     void Update()
     {
-        UnityEngine.Vector3 newPosition = new UnityEngine.Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
-     
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
         transform.position = newPosition;
-      
     }
 }
